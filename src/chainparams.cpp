3edd64ff9b275b0ade576ef93dd32b096a9b9968
yeahitsme2 <seymour40@ymail.com>
2018-10-30 20:59:09 -0400

    Updated POW Starting BlockHeight

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -138,7 +138,7 @@ public:
         nMaxMoneyOut = 21000000 * COIN;
 
         /** Height or Time Based Activations **/
-        nLastPOWBlock = 259200;
+        nLastPOWBlock = 2600001;
         nModifierUpdateBlock = 615800;
         nZerocoinStartHeight = 863787;
         nZerocoinStartTime = 1508214600; // October 17, 2017 4:30:00 AM
