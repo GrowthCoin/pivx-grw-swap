bd975910893fce72bd16d8ccb07ba52a4167c2fb
yeahitsme2 <seymour40@ymail.com>
2018-11-23 08:54:25 -0500

    Removed Pivx Genesis Hash
    Replaced with GRW genesis hash as prior was making the wallet try and index blocks from pivx chain and did not corispond with chainparams.cpp genesis hashes

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -180,7 +180,7 @@ const CBlockIndex* getexplorerBlockIndex(int64_t height)
 
 std::string getexplorerBlockHash(int64_t Height)
 {
-    std::string genesisblockhash = "0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818";
+    std::string genesisblockhash = "0x75a84e70f1403715a7d240c7dac106da5bbb1d1753a5b97e0007cec435186fa0";
     CBlockIndex* pindexBest = mapBlockIndex[chainActive.Tip()->GetBlockHash()];
     if ((Height < 0) || (Height > pindexBest->nHeight)) {
         return genesisblockhash;
